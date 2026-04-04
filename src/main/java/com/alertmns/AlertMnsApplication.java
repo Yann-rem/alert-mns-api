6204655038c21da844bbd7efdb5b86d5aff0ac06
Yannick Remy <remyyannick@sfr.fr>
2026-04-04 10:31:03 +0200

    chore(app): ajouter la classe principale de l’application Spring Boot

---
+++ src/main/java/com/alertmns/AlertMnsApplication.java
@@ -0,0 +1,15 @@
+package com.alertmns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Point d'entrée de l'application Alert MNS.
+ */
+@SpringBootApplication
+public class AlertMnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertMnsApplication.class, args);
+    }
+}
